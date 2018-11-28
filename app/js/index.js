b14b624ab68c728bc6b6f9fcc224a641eaf38c18
Tsys Artem <19successful91@gmail.com>
2018-11-28 20:39:14 +0200

    add footer

---
+++ app/js/index.js
@@ -0,0 +1,19 @@
+function initMap() {
+  var uluru = {lat: 50.4546600, lng: 30.5238000};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+function closePopup() {
+  $('.map__popup-wrapper').css({'display':'none'});
+}
+
+function upScroll() {
+  $('html, body').animate({scrollTop: 0},500);
+  return false;
+}
